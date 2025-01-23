3a8cd01399d18ad51c65822de3800f30dec59ea4
aleattene <attene.tomorrowdevs@gmail.com>
2025-01-23 16:37:33 +0100

    ci: fix react deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from './../src/components/Sidebar/Sidebar.jsx'
 
 function App() {
 
   return (
     <>
-      <div class="container">
-        <div class="box">
-          <img src="img/LOGO.jpg" width="70" height="55" />
-          <h1>Sport Management</h1>
-          <div id="button4">
-            <h2>User</h2>
-          </div>
-          <hr />
-            <h3>
-              <div id="button1">
-                <a href="collegamenti/atleti.html">Atleti</a>
-              </div>
-              <div id="button2">
-                <a href="collegamenti/tecnici.html">Tecnici</a>
-              </div>
-              <div id="button3">
-                <a href="collegamenti/medici.html">Medici</a>
-              </div>
-
-            </h3>
-        </div>
-
+      <Sidebar />
+    {/*
         <div class="image-container">
           <img src="img/immagine_federer.jpg" />
         </div>
-      </div>
+    */}
     </>
   )
 }
