f498bb620ebe4390fe21817d977bc45939cd3f3e
DaveAttene <attene.davide.git@gmail.com>
2025-01-24 13:25:47 +0100

    feat: create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div id="footer">
+         <p>&copy; 2025 Sport Management. All rights reserved.</p>
+         <p>Created by Davide Attene.</p>
+    </div>
+  );
+}
+
+export default Footer;
