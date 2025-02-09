5ca010a689c340f74c268a77b8dafa509008b6ff
aleattene <attene.tomorrowdevs@gmail.com>
2025-02-09 17:02:47 +0100

    feat: add athletes api crud calls

--- src/components/Form/AthleteForm.jsx
+++ src/components/Form/AthleteForm.jsx
@@ -4,56 +4,65 @@ import profilesService from '../../services/profilesService';
 import Button from '../Button/Button';
 import './AthleteForm.css';
 
-function AthleteForm({ isEditMode = false }) {
-    // const { id } = useParams();  // Recupera l'ID dalla URL se in modalità modifica
+function AthleteForm({ isEditMode = false, dataAthlete = {} }) {
+
     const navigate = useNavigate();
-    
-    const [athlete, setAthlete] = useState({
-        first_name: '',
-        last_name: '',
-        date_of_birth: '',
-        place_of_birth: '',
-        fiscal_code: ''
-    });
 
-    // Se in modalità modifica, carica i dati dell'atleta selezionato
-    /* useEffect(() => {
-        if (isEditMode && id) {
-            profilesService.getAthleteById(id)
-                .then(response => setAthlete(response.data))
-                .catch(error => console.error("Errore nel recupero atleta:", error));
+    // Set the initial state to empty array
+    const [athlete, setAthlete] = useState([]);
+
+    // Update the state when dataAthlete are available
+    useEffect(() => {
+        if (isEditMode && dataAthlete) {
+            setAthlete(dataAthlete);
         }
-    }, [id, isEditMode]);
-    */
+    }, [isEditMode, dataAthlete]);
 
-    // Aggiorna lo stato quando cambiano i campi
-    const handleChange = (e) => {
-        //setAthlete({ ...athlete, [e.target.name]: e.target.value });
+    // Categories 
+    const [categories, setCategories] = useState(dataCategories)
+    /*
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const data = await profilesService.getCategories();
+                setCategories(data);
+            } catch (error) {
+                console.error("Errore nel caricamento delle categorie:", error);
+            }
+        };
+    
+        fetchCategories();
+    }, []);
+    */
+   // Trainers
+   // const [trainers, setTrainers] = useState(dataTrainers)
+
+    // Update the state when fields change
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setAthlete(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
     };
 
-    // Gestisce il submit (Crea o Modifica)
-    const handleSubmit = (e) => {
-        alert("Atleta Modificato con Successo!!!")
-    /*
-        e.preventDefault();
-        if (isEditMode) {
-            // MODIFICA Atleta
-            profilesService.updateAthlete(id, athlete)
-                .then(() => {
-                    alert("Atleta aggiornato con successo!");
-                    navigate('/athletes');
-                })
-                .catch(error => console.error("Errore nell'aggiornamento:", error));
-        } else {
-            // NUOVO Atleta
-            profilesService.createAthlete(athlete)
-                .then(() => {
-                    alert("Atleta creato con successo!");
-                    navigate('/athletes');
-                })
-                .catch(error => console.error("Errore nella creazione:", error));
+    // Handle the submit (create or update athlete)
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            if (isEditMode) {
+                await profilesService.updateAthlete(athlete.id, athlete);
+                alert("Atleta aggiornato con successo!");
+            } else {
+                await profilesService.createAthlete(athlete);
+                alert("Atleta creato con successo!");
+                navigate('/athletes');
+            }
+            navigate('/athletes')
+        } catch (error) {
+            console.error("Errore nel salvataggio dell'atleta: ", error);
+            alert("Si è verificato un errore nel salvataggio dell'atleta.");
         }
-                */
     };
 
     return (
@@ -75,11 +84,32 @@ function AthleteForm({ isEditMode = false }) {
                     <input type="text" name="place_of_birth" value={athlete.place_of_birth} onChange={handleChange} required />
 
                     <label>Codice Fiscale</label>
-                    <input type="text" name="fiscal_code" value={athlete.fiscal_code} onChange={handleChange} required />
+                    <input type="text" name="fiscal_code" maxLength={16} value={athlete.fiscal_code} onChange={handleChange} required />
+
+                    <label>Categoria</label>
+                    <select name="category" value={athlete.category} onChange={handleChange} required>
+                        <option value="">Seleziona categoria</option>
+                        {categories.map(cat => (
+                            <option key={cat.id} value={cat.id}>
+                                {cat.code} - {cat.description}
+                            </option>
+                        ))}
+                    </select>
+                    {/*
+                    <label>Allenatore</label>
+                    <select name="trainer" value={athlete.trainers} onChange={handleChange} required>
+                        <option value="">Seleziona allenatore</option>
+                        {trainers.map(trainer => (
+                            <option key={trainer.id} value={trainer.id}>
+                                {trainer.first_name} {trainer.last_name}
+                            </option>
+                        ))}
+                    </select>
+                    */}
                 </div>
                 </div>
                 <div className="buttons">
-                    <Button type="submit" buttonText={isEditMode ? "Salva Modifiche" : "Crea Atleta"} />
+                    <Button type="submit" buttonText={isEditMode ? "Salva Modifiche" : "Inserisci Atleta"} />
                     <Button buttonText="Annulla" onClick={() => navigate('/athletes')} />
                 </div>
             </form>
@@ -88,3 +118,26 @@ function AthleteForm({ isEditMode = false }) {
 }
 
 export default AthleteForm;
+
+
+// Data Sample
+
+const dataCategories = [
+    { id: 1, code: 'AM', description: 'Allievi Maschile' },
+    { id: 2, code: 'AF', description: 'Allieve Femminile' },
+    { id: 3, code: 'JM', description: 'Juniores Maschile' },
+    { id: 4, code: 'JF', description: 'Juniores Femminile' },
+    { id: 5, code: 'PM', description: 'Promesse Maschile' },
+    { id: 6, code: 'PF', description: 'Promesse Femminile' },
+    { id: 7, code: 'SM', description: 'Seniores Maschile' },
+    { id: 8, code: 'SF', description: 'Seniores Femminile' }
+];
+
+
+const dataTrainers = [
+    { id: 1, first_name: "Luca", last_name: "Bianchi", fiscal_code: "LCABNC80A01H501Z" } ,
+    { id: 2, first_name: "Marco", last_name: "Rossi", fiscal_code: "MRCRSS85B02F205Y" },
+    { id: 3, first_name: "Giulia", last_name: "Verdi", fiscal_code: "GLVVRD90C03L219X" },
+    { id: 4, first_name: "Francesca", last_name: "Neri", fiscal_code: "FRANER95D04T320W" },
+    { id: 5, first_name: "Davide", last_name: "Moretti", fiscal_code: "DVDMRT99E05P123V" }
+]
--- src/services/profilesService.js
+++ src/services/profilesService.js
@@ -1,33 +1,64 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://.../profiles'; // Base URL Backend
+const API_BASE_URL = 'http://localhost:8000/';
+const API_VERSION = 'api/v1/'
+const APP = 'profiles/'
+const URL = `${API_BASE_URL}${API_VERSION}${APP}`
+
+const ATHLETES_ENDPOINT = 'athletes/'
+const CATEGORIES_ENDPOINT = 'category/'
+const TRAINERS_ENDPOINT = 'trainers/'
+const SPORT_DOCTORS_ENDPOINT = 'sport-doctors/'
+
+const API_ATHLETES = `${URL}${ATHLETES_ENDPOINT}`
+const API_CATEGORIES = `${URL}${CATEGORIES_ENDPOINT}`
+const API_TRAINERS = `${URL}${TRAINERS_ENDPOINT}`
+const API_SPORT_DOCTORS = `${URL}${SPORT_DOCTORS_ENDPOINT}`
 
 
 const profilesService = {
 
     // Read Athlets
     getAthletes: async () => {
-        // const response = await axios.get(`${API_BASE_URL}/athletes`);
-        // return response.data;
+        const response = await axios.get(`${API_ATHLETES}`);
+        return response.data;
     },
 
     // Create Athlete
     createAthlete: async (athlete) => {
-        const response = await axios.post(`${API_BASE_URL}/athletes`, athlete);
+        const response = await axios.post(`${API_ATHLETES}`, athlete);
         return response.data;
     },
 
     // Update Athlete
     updateAthlete: async (id, updatedAthlete) => {
-        const response = await axios.put(`${API_BASE_URL}/athletes/${id}`, updatedAthlete);
+        const response = await axios.put(`${API_ATHLETES}${id}/`, updatedAthlete);
         return response.data;
     },
 
     // Delete Athlete
     deleteAthlete: async (id) => {
-        const response = await axios.delete(`${API_BASE_URL}/athletes/${id}`);
+        const response = await axios.delete(`${API_ATHLETES}${id}/`);
         return response.data;
     },
+
+    // Read Athlete by ID
+    getAthleteByID: async (id) => {
+        const response = await axios.get(`${API_ATHLETES}${id}/`);
+        console.log(response)
+        return response.data;
+    },
+
+
+
+    // Read Category
+    getCategories: async () => {
+        const response = await axios.get(`${API_CATEGORIES}`);
+        console.log(API_ATHLETES)
+        console.log(response)
+        return response.data;
+    },
+
 
     // Read Trainers
     getTrainers: async () => {
@@ -39,7 +70,7 @@ const profilesService = {
         // return response.data;
     },
 
-    // Create Trainer
+    /* Create Trainer
     createTrainer: async (trainer) => {
         const response = await axios.post(`${API_BASE_URL}/trainers`, trainer);
         return response.data;
@@ -84,6 +115,7 @@ const profilesService = {
         const response = await axios.delete(`${API_BASE_URL}/sport-doctors/${id}`);
         return response.data;
     },
+    */
 
 };
 
