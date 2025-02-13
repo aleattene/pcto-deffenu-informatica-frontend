1d6ed29c37e715ddad1c8ec2b65dbd82907f9983
aleattene <attene.tomorrowdevs@gmail.com>
2025-02-13 16:37:15 +0100

    refactor: add tailwind logo with redirect to homepage

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import "./Logo.css";
+import { useNavigate } from 'react-router-dom';
 import logo from './../../assets/img/logo.svg'
 
-function LogoWithText(props) {
 
-  const {onClick} = props;
+function LogoWithText() {
 
-  return (
-    <div id="container-logo" onClick={onClick}>
-      {/* Logo */}
-      <img id="logo-img"
-        /* Path da cui recuperare la risorsa */
-        src={logo} 
-        /* Placeholder che viene scritto se immagine non trovata */
-        alt="Logo SM" 
-      />
-      {/* Testo */}
-      <span id="logo-description">Sport Management </span> 
-    </div>
-  );
+    // Redirect to Homepage
+    const navigate = useNavigate()
+    const goToHomepage = () => navigate('/')
+
+    return (
+        <div className="flex items-center justify-center mt-8 cursor-pointer" onClick={goToHomepage}>
+            <div class="flex items-center mx-4" >
+                <img id="logo-img" className='w-13 h-13 rounded-full' src={logo} alt="Logo SM"
+                />
+                <span class="mx-4 text-2xl font-semibold text-gray-300">Sport Management</span>
+            </div>
+        </div>
+    );
 }
 
 export default LogoWithText;
