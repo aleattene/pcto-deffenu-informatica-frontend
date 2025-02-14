eb4087c5fc6ff8145a73eafad1507ec3d4202b64
aleattene <attene.tomorrowdevs@gmail.com>
2025-02-15 00:19:15 +0100

    fix: improve get started button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,13 +4,18 @@ import { Link } from "react-router-dom";
 function Homepage() {
   return (
     <div className="bg-gradient-to-b from-blue-600 to-orange-400 w-full h-screen flex flex-col items-center relative">
-      <div className="absolute top-1/6 left-1/2 -translate-x-1/2 z-10">
+      <div className="absolute bottom-[15%] left-1/2 lg:left-1/3 -translate-x-1/2 z-10">
+
         <Link
-          to="/athletes"
-          className="bg-blue-600 shadow-xl outline-3 outline-gray-700
-          px-6 py-3 rounded-md text-2xl font-bold transition duration-300 hover:bg-orange-400 hover:text-white hover:scale-110"
+          to="/athletes">
+            <button
+          className="bg-blue-600 text-gray-200 font-bold shadow-xl outline-2 outline-gray-600
+          px-6 py-3 rounded-md text-xl transition duration-300 hover:bg-orange-400 hover:text-white
+          w-screen max-w-[200px]
+          " 
         >
           GET STARTED
+          </button>
         </Link>
       </div>
       <img
