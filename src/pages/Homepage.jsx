51d7c6212082e0f3cf3f86197b066b48d2941eda
aleattene <attene.tomorrowdevs@gmail.com>
2025-02-15 17:25:35 +0100

    style: remove bad shadow

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ function Homepage() {
     <div className="bg-gradient-to-b from-blue-600 to-orange-400 w-full h-screen flex flex-col items-center relative">
       
       <div className="absolute top-[10%] left-1/2 -translate-x-1/2 z-10 block text-center min-w-80 max-w-full">
-        <h1 className="text-[#E5E7EB] font-bold text-2xl mb-2 shadow">
+        <h1 className="text-[#E5E7EB] font-bold text-2xl mb-2">
           ATTENE DAVIDE
         </h1>
         <h3 className="text-[#E5E7EB] font-semibold text-md mb-2">
